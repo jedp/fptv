569395bc81f74b32139715ec5cda3f019b1f0b5f
Jed Parsons <jed@jedparsons.com>
2025-12-30 15:40:19 -0800

    redirect logs to system journal, if available (Linux vs Mac)

--- python/fptv/log.py
+++ python/fptv/log.py
@@ -1,15 +1,29 @@
 import sys
 
+try:
+    from systemd import journal
+
+    # Linux
+    _HAS_JOURNAL = True
+except ImportError:
+    # Mac
+    _HAS_JOURNAL = False
+
 
 class Logger:
     def __init__(self, tag: str):
         if tag is None:
             raise ValueError("Missing required argument 'tag'")
-
         self.tag = tag
 
     def out(self, msg: str) -> None:
-        print(f"[{self.tag}] {msg}")
+        if _HAS_JOURNAL:
+            journal.send(f"{msg}", SYSLOG_IDENTIFIER=self.tag)
+        else:
+            print(f"[{self.tag}] {msg}")
 
     def err(self, msg: str) -> None:
-        print(f"[{self.tag}] ERROR: {msg}", file=sys.stderr)
+        if _HAS_JOURNAL:
+            journal.send(f"{msg}", PRIORITY=journal.LOG_ERR, SYSLOG_IDENTIFIER=self.tag)
+        else:
+            print(f"[{self.tag}] ERROR: {msg}", file=sys.stderr)
